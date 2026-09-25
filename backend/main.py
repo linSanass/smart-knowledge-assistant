68d1acd256645bc9a35be29c59749062ee72ff71
linSanass <1601529252@qq.com>
2026-09-25 12:35:40 +0800

    feat: integrate DeepSeek AI chat

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,22 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+# 加载.env
+load_dotenv()
+
+# 创建DeepSeek客户端
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
 app = FastAPI()
 
+# 允许React访问
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -12,6 +26,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# 请求数据格式
 class ChatRequest(BaseModel):
     message: str
 
@@ -23,6 +38,19 @@ def root():
 
 @app.post("/chat")
 def chat(req: ChatRequest):
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {
+                "role": "user",
+                "content": req.message
+            }
+        ]
+    )
+
+    answer = response.choices[0].message.content
+
     return {
-        "answer": f"你刚刚说的是：{req.message}"
+        "answer": answer
     }
